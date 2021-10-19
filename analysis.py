0c53afbf58f829c2280fbb60793c65c6206f618f
Stefanos Klironomos <kliron@tuta.io>
2021-10-19 22:51:13 +0200

    Working on pulling a volume on a segmentation

--- analysis.py
+++ analysis.py
@@ -5,18 +5,22 @@ import json
 import numpy
 
 # By default simpleitk will try to use the Fiji program to show images, change to slicer
-os.environ['SITK_SHOW_COMMAND'] = '/Applications/Slicer.app/Contents/MacOS/Slicer'
+os.environ['SITK_SHOW_COMMAND'] = 'H:/Slicer 4.11.20210226/Slicer.exe' if os.name == 'nt' else \
+    '/Applications/Slicer.app/Contents/MacOS/Slicer'
+default_dicom_tags_json_path = 'H:/Dokument/Projects/DSC_COVID19/dicom_tags.json' if os.name == 'nt' else \
+    './dicom_tags.json'
+segmentations_dir = 'H:/Dokument/Projects/segmentations' if os.name == 'nt' else '/Users/kliron/Data/dsc_covid19/segmentations'
 
-default_dicom_tags_json_path = './dicom_tags.json'
 
-
-def get_dicom_tags(json_path: str = default_dicom_tags_json_path) -> dict:
+def get_dicom_tags(json_path: str = default_dicom_tags_json_path, invert=False) -> dict:
     """
-    Reads a dictionary of DICOM tag to human readable tag description
+    Reads a dictionary of DICOM tag to human readable tag description.
+    if `invert` is True, switch key-value pairs
     """
     with open(json_path, 'r') as r:
         js = r.read()
-        return json.loads(js)
+        t = json.loads(js)
+        return t if not invert else {v: k for k, v in t.items()}
 
 
 # Flip Angle -> '0018|1314'
@@ -96,52 +100,43 @@ def read_4d_series(dsc_path: str) -> [(sitk.Image, dict)]:
         else:
             file_lists[acquisition_number-1].append(dcm_name)
 
-    tags = get_dicom_tags()
-
     series_list = []
     for dcm_names_list in file_lists:
         reader.SetFileNames(dcm_names_list)
-        metadata = {}
         reader.LoadPrivateTagsOn()
         reader.MetaDataDictionaryArrayUpdateOn()
         series = reader.Execute()
-        for k in reader.GetMetaDataKeys(slice=0):
-            try:
-                desc = tags[k]
-            except KeyError:
-                print(f'DICOM tag {k} is missing a descriptive name')
-                desc = k
-            metadata[desc] = reader.GetMetaData(0, k)
-
+        metadata = {k: reader.GetMetaData(0, k) for k in reader.GetMetaDataKeys(slice=0)}
         series_list.append((series, metadata))
 
     return series_list
 
 
-def register(ser1: sitk.Image, ser2: sitk.Image) -> sitk.Image:
-    """
-
-    :param ser1:
-    :param ser2:
-    :return:
-    """
-
-
 if __name__ == '__main__':
-    # dicom_dir = '/Users/kliron/Downloads/tmp/data/DICOM/0000931B/AAC70388/AA080C75'
-    dicom_dir = sys.argv[1]
-    # ser, meta = read_3d_series(os.path.join(dicom_dir, '000055DE'))
-    data = read_4d_series(os.path.join(dicom_dir, '000055DE'))
-    sitk.Show(data[7][0], title="First frame of series", debugOn=False)
+    dicom_dir = 'H:/Dokument/Projects/C19Perf/SEKBF00012197064/DICOM/0000294F/AACBE554/AA0C1C1E' if os.name == 'nt' else \
+        '/Users/kliron/Downloads/tmp/data/DICOM/0000931B/AAC70388/AA080C75'
 
-    print((f'Sequence: {meta["Sequence Name"]}\n'
-           f'FA: {meta["Flip Angle"]}\n'
-           f'TE: {meta["Echo Time"]}\n'
-           f'TR: {meta["Repetition Time"]}\n'
-           f'MFS: {meta["Magnetic Field Strength"]}\n'
-           f'Matrix: {meta["Acquisition Matrix"]}'))
+    img = read_4d_series(os.path.join(dicom_dir, '000055DE'))
+    meta = img[0][1]
+    tags = get_dicom_tags(invert=True)
 
-    get_tag_by_name('Content Time')
-    # Convert to numpy
-    # arr = sitk.GetArrayFromImage(ser)
+    print((f'Sequence: {meta[tags["Sequence Name"]]}\n'
+           f'FA: {meta[tags["Flip Angle"]]}\n'
+           f'TE: {meta[tags["Echo Time"]]}\n'
+           f'TR: {meta[tags["Repetition Time"]]}\n'
+           f'MFS: {meta[tags["Magnetic Field Strength"]]}\n'
+           f'Matrix: {meta[tags["Acquisition Matrix"]]}'))
+
+    seg = sitk.ReadImage(os.path.join(segmentations_dir, 'Plexus.nrrd'))
+
+    overlay = sitk.LabelOverlay(img[7][0], seg, opacity=0.01)
+    sitk.Show(overlay, title='Overlay')
+
+    # This won't do, only computes rectangular bounding box
+    # shape = sitk.LabelShapeStatisticsImageFilter()
+    # shape.ComputeOrientedBoundingBoxOn()
+    # shape.Execute(seg)
+    # new_im = sitk.RegionOfInterest(img[7][0], shape)
+    
+    sitk.Show(new_im, title='Segmented volume')
 
