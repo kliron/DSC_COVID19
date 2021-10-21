22f1d5dfb39dfa2df7d26654141fd21a23d281da
Stefanos Klironomos <kliron@tuta.io>
2021-10-21 19:49:25 +0200

    Refactorings

--- analysis.py
+++ analysis.py
@@ -1,14 +1,25 @@
+# -*- coding: utf-8 -*-
 import SimpleITK as sitk
 import os
 import sys
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-# By default simpleitk will try to use the Fiji program to show images, change to slicer
+# This is to make relative imports work both when this file is run "on the fly" in an IDE
+# and when it is run as a script (as __main__). For the first case to work, the console
+# must of course cd in the directory this file is in.
+parent_module = sys.modules['.'.join(__name__.split('.')[:-1]) or '__main__']
+if __name__ == '__main__' or parent_module.__name__ == '__main__':
+    from paths import paths
+else:
+    from . import paths
+
+
 os.environ['SITK_SHOW_COMMAND'] = 'H:/Slicer 4.11.20210226/Slicer.exe' if os.name == 'nt' else \
     '/Applications/Slicer.app/Contents/MacOS/Slicer'
-default_dicom_tags_json_path = 'H:/Dokument/Projects/DSC_COVID19/dicom_tags.json' if os.name == 'nt' else \
+default_dicom_tags_json_path = 'H:/Dokument/Projects/dsc_covid19/dicom_tags.json' if os.name == 'nt' else \
     './dicom_tags.json'
 ACQUISITION_NUMBER_DICOM_TAG = '0020|0012'
 
@@ -16,25 +27,13 @@ ACQUISITION_NUMBER_DICOM_TAG = '0020|0012'
 def get_dicom_tags(json_path: str = default_dicom_tags_json_path, invert=False) -> dict:
     """
     Reads a dictionary of DICOM tag to human readable tag description.
-    if `invert` is True, switch key-value pairs
+    if `invert` is True, the key-value pairs are switched
     """
     with open(json_path, 'r') as r:
         tags = json.loads(r.read())
         return tags if not invert else {val: key for key, val in tags.items()}
 
 
-# Flip Angle -> '0018|1314'
-# Echo Time -> '0018|0081'
-# Repetition Time -> '0018|0080'
-# Inversion Time -> '0018|0082'
-# Sequence Name -> '0018|0024'
-# Magnetic Field Strength -> '0018|0087'
-# Echo Train Length -> '0018|0091'
-# In-plane Phase Encoding Direction -> '0018|1312'
-# Acquisition Matrix -> '0018|1310'
-# Transmit Coil Name -> '0018|1251'
-# Receive Coil Name -> '0018|1250'
-# Content Time -> '0008|0033'
 def get_tag_by_name(name: str) -> str:
     """
     :param name: Tag human readable description
@@ -106,53 +105,50 @@ def read_4d_series(dsc_path: str) -> [(sitk.Image, dict)]:
     return series_list
 
 
-def main():
+def img_histogram(img: sitk.Image, bins=None) -> None:
     """
-    Segmentations are saved as labelmaps where each voxel belonging to the segmentation has the value 1 (default)
-    and the rest the value 0. Thus, by using orig_img_arr[segm==1] we can index into the original image and get the
-    voxels corresponding to the volume of the segmentation.
+    Get a histogram of voxel intensities for an image
+    """
+    plt.hist(sitk.GetArrayFromImage(img).flatten(), bins=bins)
 
+
+def show_overlay(bg_img: sitk.Image, overlayed_img: sitk.Image, opacity: float = 0.01) -> None:
+    sitk.Show(sitk.LabelOverlay(bg_img, overlayed_img, opacity=opacity), title='Overlay')
+
+
+def edit_segmentation(uid: str,
+                      params: dict[str:str],
+                      data_root: str = 'H:/Dokument' if os.name == 'nt' else '/Users/kliron',
+                      dicom_root: str = 'Data/dsc_covid19/Examinations',
+                      segmentations_dir: str = 'Data/dsc_covid19/Segmentations',
+                      segmentation_file: str = 'Plexus.nrrd',
+                      foreground_label: int = 1,
+                      background_label: int = 0,
+                      show: bool = False) -> None:
+    """
+    Removes susceptibility artifacts and non-enhancing pixels from a segmentation volume. Writes the new segmentation
+    at the path specified as `os.path.join(data_root, segmentations_dir, uid, f'final_{segmentation_file}').
     :return: None
     """
-    data_root = 'H:/Dokument' if os.name == 'nt' else '/Users/kliron'
-    dicom_root = 'Data/dsc_covid19/Examinations'
-    segmentations_dir = 'Data/dsc_covid19/Segmentations'
-    segmentation_file = 'Plexus.nrrd'
-    segmentation_label = 1  # default
-    background_label = 0    # default
-    with open('./paths.json') as r:
-        js = r.read()
-        dicom_dirs = json.loads(js)
-    [(uid, params)] = dicom_dirs.items()
     dicom_path = os.path.join(data_root, dicom_root, uid, 'DICOM', params['dicom_dir'])
     data = read_4d_series(dicom_path)
     segm = sitk.ReadImage(os.path.join(data_root, segmentations_dir, uid, segmentation_file))
+
     # Get the average voxel values for the non-contrast series
     noncontrast_imgs = [data[frame][0] for frame in params['noncontrast_frames']]
     contrast_imgs = [data[frame][0] for frame in params['contrast_frames']]
-
-    # To see the segmentation overlayed on the actual frame:
-    # sitk.Show(sitk.LabelOverlay(noncontrast_imgs[0], segm, opacity=0.01), title='Overlay')
-
-    #
-    # From the  Bouzerar et al paper (Neuroradiology 2013, DOI 10.1007/s00234-013-1290-2):
-    # "For each pixel, the pre-bolus baseline S0 was estimated by
-    #  averaging 10 to 12 points before tB. For an individual voxel,
-    #  significant enhancement occurred when the mean signal intensity
-    #  over the final ten time points was greater (<-what??) than 1 standard
-    #  deviation over the voxel average baseline S0. After comparison
-    #  between the post-bolus signal and the pre-bolus baseline S0,
-    #  pixels within the mask that did not demonstrate significant signal
-    #  enhancement were categorized as non-enhancing pixels and
-    #  included for the calculation of ΔR*(t)."
-    #
     noncontrast_avg_array = np.round(np.add(*[sitk.GetArrayFromImage(img) for img in noncontrast_imgs])/len(noncontrast_imgs)).astype(int)
     contrast_avg_array = np.round(np.add(*[sitk.GetArrayFromImage(img) for img in contrast_imgs])/len(contrast_imgs)).astype(int)
 
-    # To edit the segmentation itself, we save the indexes of each one of its voxels:
+    # To edit the segmentation itself, we save the indexes of each one of its voxels
     segm_arr = sitk.GetArrayFromImage(segm)
-    mask_indexes = list(zip(*np.where(segm_arr == segmentation_label)))
+    mask_indexes = list(zip(*np.where(segm_arr == foreground_label)))
 
+    #
+    # Segmentations are saved as labelmaps where each voxel belonging to the segmentation has the value 1 (default)
+    # and the rest the value 0. Thus, by using orig_img_arr[segm == 1] we can index into the original image and get the
+    # voxels corresponding to the volume of the segmentation.
+    #
     # Remove voxels whose noncontrast minus contrast average intensity is lower than one standard deviation of the
     # average noncontrast intensity.
     remove_condition = (noncontrast_avg_array[segm_arr == 1] - contrast_avg_array[segm_arr == 1]) < np.std(noncontrast_avg_array[segm_arr == 1])
@@ -166,11 +162,30 @@ def main():
     final_segm = sitk.GetImageFromArray(segm_arr)
     final_segm.CopyInformation(segm)
 
-    # Look at the final segmentation
-    sitk.Show(sitk.LabelOverlay(noncontrast_imgs[0], final_segm, opacity=0.01))
-    sitk.WriteImage(final_segm, os.path.join(data_root, segmentations_dir, uid, 'Plexus_final.nrrd'))
+    # Erosion currently does not work, sets all pixels to 0.
+    # To reduce partial volume effects, erode the segmentation borders
+    # f = sitk.BinaryErodeImageFilter()
+    # f.SetKernelType(sitk.sitkBall)
+    # f.SetKernelRadius(1)
+    # f.SetBackgroundValue(0)
+    # f.SetForegroundValue(1)
+    # final_segm: sitk.Image = f.Execute(final_segm)
+
+    # len(np.where(sitk.GetArrayFromImage(final_segm) == 1)[0])
+
+    if show:
+        show_overlay(noncontrast_imgs[0], final_segm)
+
+    sitk.WriteImage(final_segm, os.path.join(data_root, segmentations_dir, uid, f'final_{segmentation_file}'))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='DSC perfusion analysis.')
+    
+    items = paths.items()
+    for uid, params in items:
+        edit_segmentation(uid, params)
 
 
 if __name__ == '__main__':
     main()
-
---
+++ paths.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+paths = {
+  'SEKBF00012197064': {
+    'dicom_dir': '0000294F/AACBE554/AA0C1C1E/0000376F',
+    'noncontrast_frames': [0, 1, 2],
+    'contrast_frames': [4, 5, 6]
+  }
+}
